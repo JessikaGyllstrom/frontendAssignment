1105c477853a9733b467745246f72cf29b90296d
Jessika Gyllstrom <jessikagyllstrom@gmail.com>
2023-04-07 03:29:11 +0200

    animation added when inputfield is active

---
+++ myscript.js
@@ -0,0 +1,25 @@
+
+// Get the container element
+var inputContainer = document.getElementById("text");
+// Get all inputfields inside the container
+let input = inputContainer.getElementsByClassName("input");
+
+
+for (var i = 0; i < input.length; i++) {
+  input[i].addEventListener("focus", function() {
+    console.log("active");
+    rocket.classList.add("start");
+    // Add the active class to the current inputfield
+    this.className += " active";
+  })
+  input[i].addEventListener('focusout',  function(ev, data) {
+    let rocket = document.getElementById("rocket");
+    var current = document.getElementsByClassName("active");
+  // If there's no active class
+  if (current.length > 0) { 
+    current[0].className = current[0].className.replace("active", "notactive");
+    rocket.classList.remove("start");
+    console.log("no active class");
+  }}
+)}
+
